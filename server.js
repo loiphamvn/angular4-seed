3b660280c57da008d5114241fef6f2f6ac1a3042
Le Duong <me@leduong.com>
2017-04-28 15:01:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
 
 global.base = __dirname;
-app.use("/", express.static(__dirname + "/dist"));
+app.use("/", express.static(path.join(__dirname,'dist')));
 
 app.listen(port, '0.0.0.0', function() {
   console.log('Listening on port %d', port);
